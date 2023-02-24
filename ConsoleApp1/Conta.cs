5ee3e00173e5f4b58759a17cbbe3af7fd58a005c
047106 <047106@UNI-LAB2-32>
2023-02-23 21:08:34 -0300

    Add project files.

---
+++ ConsoleApp1/Conta.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO
+{
+    public class Conta
+    {
+        public string? Nome;
+        public string? NumeroConta;
+        public double Saldo;
+
+        public void Depositar(double valor)
+        {
+            Saldo += valor;
+        }
+        public void Sacar(double valor)
+        {
+            Saldo -= valor;
+        }
+    }
+}
